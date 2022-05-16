d261393dce8a8232ccc24cf235655ad347537ab5
clive <98222803+AAClive@users.noreply.github.com>
2022-05-16 18:15:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 from flask import Flask
+from flask import flash
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
-
 app=Flask(__name__)
 db=SQLAlchemy(app)
 DB_NAME="database.db"
+#datra base init
 app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///{DB_NAME}'
-db.init_app(app)
+db.SQLlchemy(app)
 
-class users(db.Model,UserMixin):
+class Users(db.Model):
     id=db.Column("id", db.Integer, primary_key=True)
     email=db.Column(db.String(100),unique=True)
     password=db.Column(db.String(100))
-    
     def __init__(self, email):
         self.email=email
-        
 @app.route("/",methods=["GET","POST"])
 def siginup():
     if request.method=="POST":
         user=request.form.get("email")
         pas=request.form.get("password")
-        
+        if user or pas==None:
+            flash("[SERVER] invalid email or password, try again!")
+            user=Users()
     return render_template("signup.html")
         
 
